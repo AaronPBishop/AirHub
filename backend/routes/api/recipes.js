ad5989ef16bd1ff1a5538470135f4869a69436e0
AaronPBishop <aaronpbishop@gmail.com>
2023-04-07 21:33:41 -0500

    Resolved incorrect config path in models/index.js file - Fixed misc. '/recipes' route issues

--- backend/routes/api/recipes.js
+++ backend/routes/api/recipes.js
@@ -2,15 +2,14 @@ const express = require('express');
 const { Sequelize, Op } = require('sequelize');
 
 const { requireAuth } = require('../../utils/auth.js');
-const { validateQueryParameters } = require('../../utils/validation');
 
 const { Recipe } = require('../../db/models');
 
 const router = express.Router();
 
 // Get All Recipes
-router.get('/', validateQueryParameters, async (req, res) => {
-    const allRecipes = await Recipe.findAll({ attributes: ['id', 'ownerId', 'brand', 'item', 'cookTime', 'cookTemp', 'notes', 'avgRating', 'previewImage'] });
+router.get('/', async (req, res) => {
+    const allRecipes = await Recipe.findAll({ attributes: ['id', 'ownerId', 'brand', 'item', 'cookTime', 'cookTemp', 'notes', 'avgRating', 'previewImg'] });
 
     return res.json({ Recipes: allRecipes });
 });
